f815c180b70a8211aa3751ff6c41ff0468a8766b
Matt <matthew.ingram@durham.ac.uk>
2018-11-18 10:40:26 +0000

    Cleanup up repo, staged required files, changed name of profile to profileBase

--- runall.sh
+++ runall.sh
@@ -3,10 +3,10 @@ for mat in mat/*.matrix
 do
 	[ -e "$mat" ] || continue
 	matName=$(echo $mat | cut -d'/' -f2 | cut -d'.' -f1)
-	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${matName}_X_${matName}.matrix mat/${matName}.matrix mat/${matName}.matrix" >> profileall.slurm
-	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${matName}_X_${matName}.matrix mat/${matName}.matrix mat/${matName}.matrix" 
-	sbatch -n 1 -p test.q -o ./output/${matName}_X_${matName}.txt profileall.slurm
-	sed -i '$ d' profileall.slurm
+	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${matName}_X_${matName}.matrix mat/${matName}.matrix mat/${matName}.matrix" >> profileBase.slurm
+	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${matName}_X_${matName}.matrix mat/${matName}.matrix mat/${matName}.matrix" >> profileBase.slurm
+	sbatch -n 1 -p test.q -o ./output/${matName}_X_${matName}.txt profileBase.slurm
+	sed -i '$ d' profileBase.slurm
 done
 
 for massMat in mat/mass/*.matrix
@@ -18,7 +18,7 @@ do
 	else
 		laplaceMatName=${massMatName//"mass"/"laplace"}
 	fi
-	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${massMatName}_X_${laplaceMatName}.matrix mat/${massMatName}.matrix mat/${laplaceMatName}.matrix" >> profileall.slurm
-	sbatch -n 1 -p test.q -o ./output/${massMatName}_X_${laplaceMatName}.txt profileall.slurm
-	sed -i '$ d' profileall.slurm 
+	echo "likwid-perfctr -f -C 0 -g DATA -m ./profile results/${massMatName}_X_${laplaceMatName}.matrix mat/${massMatName}.matrix mat/${laplaceMatName}.matrix" >> profileBase.slurm
+	sbatch -n 1 -p test.q -o ./output/${massMatName}_X_${laplaceMatName}.txt profileBase.slurm
+	sed -i '$ d' profileBase.slurm 
 done
---
+++ runcsv.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+mat1=$(echo $1 | cut -d'/' -f2 | cut -d'.' -f1)
+mat2=$(echo $2 | cut -d'/' -f2 | cut -d'.' -f1)
+echo "likwid-perfctr -f -C 0 -g $3 -m ./profile results/${mat1}_X_${mat2}.matrix mat/${mat1}.matrix mat/${mat2}.matrix" >> profileall.slurm
+sbatch -n 1 -p test.q -t $4 -O -o ./output/${mat1}_X_${mat2}.csv profileall.slurm
+sed -i '$ d' profileall.slurm
+
+#To Use: $1 - mat/(file.matrix), $2 - mat/(file.matrix), $3 - event
